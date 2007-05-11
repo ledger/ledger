643f666468d3e378cc0b39e501c253e33c267f0f
John Wiegley <johnw@newartisans.com>
2007-05-11 09:12:17 +0000

    Set DYLD_LIBRARY_PATH to find locally built dynamic libraries.

--- tests/python/PyUnitTests.py
+++ tests/python/PyUnitTests.py
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
+DYLD_LIBRARY_PATH="%builddir%/.libs":"%builddir%/gdtoa/.libs":$DYLD_LIBRARY_PATH \
     python "%srcdir%"/tests/python/UnitTests.py
