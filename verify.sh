7059d5d8ca8f3c120946b29b8d97f2f616b79b95
John Wiegley <johnw@newartisans.com>
2007-05-11 07:20:54 +0000

    Added --local option to acprep.

---
+++ verify.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+TMPDIR=$HOME/tmp
+
+if [ -d $HOME/src/ledger/.git ]; then
+    LEDGER_GIT=$HOME/src/ledger
+else
+    LEDGER_GIT=http://newartisans.com/ledger.git
+fi
+
+cd $TMPDIR
+
+mkdir ledger || exit 1
+
+cd ledger
+git clone $LEDGER_GIT local_git || exit 1
+
+git clone -l local_git distcheck || exit 1
+cd distcheck || exit 1
+./acprep --local || exit 1
+make distcheck || exit 1
+
+function build_ledger() {
+    name=$1
+    shift 1
+
+    cd $TMDIR/ledger || exit 1
+    git clone -l local_git $name || exit 1
+    cd $name || exit 1
+
+    ./acprep --local "$@" || exit 1
+
+    (cd gdtoa && make) || exit 1
+    make || exit 1
+    make fullcheck || exit 1
+}
+
+build_ledger(normal)
+build_ledger(devel, --devel)
+build_ledger(python, --python)
+
+build_ledger(debug, --debug)
+build_ledger(boost_debug, --debug, --boost, d)
+build_ledger(debug_python, --debug, --python)
+
+build_ledger(optimized, --opt)
+build_ledger(opt_python, --opt, --python)
+
+rm -fr $TMPDIR/ledger || exit 1
+
+exit 0
