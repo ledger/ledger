4fe230eb1f73e9e3398e82138812b25ddc165968
John Wiegley <johnw@newartisans.com>
2007-05-11 07:24:13 +0000

    Corrected configure.in.

--- verify.sh
+++ verify.sh
@@ -21,17 +21,19 @@ mkdir ledger || exit 1
 cd ledger || exit 1
 git clone $LEDGER_GIT local_git || exit 1
 
-git clone -l local_git distcheck || exit 1
-cd distcheck || exit 1
-./acprep --local || exit 1
-make CPPFLAGS="-I/usr/local/include -I/usr/local/include/boost -I/sw/include" \
-     LDFLAGS="-L/usr/local/lib -L/sw/lib" distcheck || exit 1
+function build_distcheck() {
+    git clone -l local_git distcheck || exit 1
+    cd distcheck || exit 1
+    ./acprep --local || exit 1
+    make CPPFLAGS="-I/usr/local/include -I/usr/local/include/boost -I/sw/include" \
+         LDFLAGS="-L/usr/local/lib -L/sw/lib" distcheck || exit 1
+}
 
 function build_ledger() {
     name=$1
     shift 1
 
-    cd $TMDIR/ledger || exit 1
+    cd $TMPDIR/ledger || exit 1
     git clone -l local_git $name || exit 1
     cd $name || exit 1
 
@@ -42,15 +44,17 @@ function build_ledger() {
     make fullcheck || exit 1
 }
 
-build_ledger(normal)
-build_ledger(devel, --devel)
-build_ledger(python, --python)
+#build_distcheck
 
-build_ledger(debug, --debug)
-build_ledger(boost_debug, --debug, --boost, d)
-build_ledger(debug_python, --debug, --python)
+build_ledger normal
+build_ledger devel --devel
+build_ledger python --python
 
-build_ledger(optimized, --opt)
-build_ledger(opt_python, --opt, --python)
+build_ledger debug --debug
+#build_ledger boost_debug --debug --boost d
+build_ledger debug_python --debug --python
+
+build_ledger optimized --opt
+build_ledger opt_python --opt --python
 
 exit 0
