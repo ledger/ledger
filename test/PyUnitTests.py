6ddc21dc5fe75c80df1bf383e5bfc2320deb8624
John Wiegley <johnw@newartisans.com>
2008-08-06 23:21:06 -0400

    'make distcheck' now works.

--- test/PyUnitTests.py
+++ test/PyUnitTests.py
@@ -2,4 +2,4 @@
 
 PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
 DYLD_LIBRARY_PATH="%builddir%/.libs":"%builddir%/gdtoa/.libs":$DYLD_LIBRARY_PATH \
-    python "%srcdir%"/tests/python/UnitTests.py
+    python "%srcdir%"/test/UnitTests.py
