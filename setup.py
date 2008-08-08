0db8d69796d1d01eb25026efc2316c20a29f5d79
John Wiegley <johnw@newartisans.com>
2008-08-08 02:50:05 -0400

    The Boost.Python module is building again.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup, Extension
 
 import os
 
-libs = ["amounts", "boost_python", "gmp"]
+defines = [('PYTHON_MODULE', 1)]
+libs    = ["amounts", "boost_python", "gmp"]
 
 setup(name         = "Amounts",
       version      = "2.6.1",
@@ -13,6 +14,6 @@ setup(name         = "Amounts",
       author_email = "johnw@newartisans.com",
       url          = "http://www.newartisans.com/johnw/",
       ext_modules  = [
-    Extension("amounts", ["amounts.cc"],
+    Extension("amounts", [os.path.join(os.environ['SRCDIR'], "amounts.cc")],
 	      define_macros = [('PYTHON_MODULE', 1)],
 	      libraries     = libs)])
