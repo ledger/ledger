d6fd0cd24f6d1c67f8456ad3f394c3ea9dc7afc4
John Wiegley <johnw@newartisans.com>
2008-08-08 18:49:50 -0400

    Removed a redundant comment.

--- python/pyledger.h
+++ python/pyledger.h
@@ -32,15 +32,6 @@
 #ifndef _PYLEDGER_H
 #define _PYLEDGER_H
 
-//////////////////////////////////////////////////////////////////////
-//
-// Ledger Accounting Tool (with Python support via Boost.Python)
-//
-//   A command-line tool for general double-entry accounting.
-//
-// Copyright (c) 2003,2004 John Wiegley <johnw@newartisans.com>
-//
-
 #include <ledger.h>
 #include <pyinterp.h>
 
