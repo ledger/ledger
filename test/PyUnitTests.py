3f7430bd81774169d99e2337f1d7263c3301d00f
John Wiegley <johnw@newartisans.com>
2008-08-08 03:13:29 -0400

    Don't include gdtoa/.libs anymore, it's not being used.

--- test/PyUnitTests.py
+++ test/PyUnitTests.py
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
-DYLD_LIBRARY_PATH="%builddir%/.libs":"%builddir%/gdtoa/.libs":$DYLD_LIBRARY_PATH \
+DYLD_LIBRARY_PATH="%builddir%/.libs":$DYLD_LIBRARY_PATH \
     python "%srcdir%"/test/UnitTests.py
