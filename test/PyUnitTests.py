572a37a9c61bc6678aaee9990361d41140a65415
John Wiegley <johnw@newartisans.com>
2008-08-10 00:56:28 -0400

    Use $PYTHON to locate which Python interpretor to use during a build.

--- test/PyUnitTests.py
+++ test/PyUnitTests.py
@@ -2,4 +2,4 @@
 
 PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
 DYLD_LIBRARY_PATH="%builddir%/.libs":$DYLD_LIBRARY_PATH \
-    python "%srcdir%"/test/UnitTests.py
+    %python% "%srcdir%"/test/UnitTests.py
