3fc2bf263f71fbec6c3d066132d64af83cef718a
John Wiegley <johnw@newartisans.com>
2008-08-10 05:16:19 -0400

    Committed initial attempt at making a standalone installer for OS X.

---
+++ dist/pkg/post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf /usr/lib/libexpat.1.5.2.dylib /usr/lib/libexpat.1.dylib
+ln -sf /usr/lib/libexpat.1.5.2.dylib /usr/lib/libexpat.dylib
+
+ln -sf /usr/lib/libgmp.3.4.2.dylib /usr/lib/libgmp.3.dylib
+ln -sf /usr/lib/libgmp.3.4.2.dylib /usr/lib/libgmp.dylib
+
