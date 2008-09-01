855ac383d8b0bfc6de294f6c33516bb77138163e
John Wiegley <johnw@newartisans.com>
2008-08-31 21:01:19 -0400

    Set LD_LIBRARY_PATH in addition to DYLD_LIBRARY_PATH.

--- test/PyUnitTests.py
+++ test/PyUnitTests.py
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
+LD_LIBRARY_PATH="%builddir%/.libs":$LD_LIBRARY_PATH \
 DYLD_LIBRARY_PATH="%builddir%/.libs":$DYLD_LIBRARY_PATH \
     %python% "%srcdir%"/test/UnitTests.py
