07fcc3a08af552bd40d4bd5103d4668662acdaf4
John Wiegley <johnw@newartisans.com>
2009-02-24 12:48:11 -0400

    Build Python module using automake, not setup.py

--- test/PyUnitTests.py
+++ test/PyUnitTests.py
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PYTHONPATH="%builddir%":"%srcdir%":$PYTHONPATH \
+PYTHONPATH="%builddir%/.libs":$PYTHONPATH \
 LD_LIBRARY_PATH="%builddir%/.libs":$LD_LIBRARY_PATH \
 DYLD_LIBRARY_PATH="%builddir%/.libs":$DYLD_LIBRARY_PATH \
     %python% "%builddir%"/test/python/UnitTests.py
