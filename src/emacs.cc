d2915c6625d6d21d7d583797da8eea77ee608e3a
John Wiegley <johnw@newartisans.com>
2012-01-11 17:55:15 -0600

    Fixed doubled quotes in pathnames in emacs report
    Fixes #580

--- src/emacs.cc
+++ src/emacs.cc
@@ -41,7 +41,7 @@ namespace ledger {
 void format_emacs_posts::write_xact(xact_t& xact)
 {
   if (xact.pos)
-    out << "\"" << xact.pos->pathname << "\" "
+    out << "\"" << xact.pos->pathname.string() << "\" "
         << xact.pos->beg_line << " ";
   else
     out << "\"\" " << -1 << " ";
