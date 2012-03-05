f9de33d21cd62d953147d62e01958557562a1311
John Wiegley <johnw@newartisans.com>
2012-03-05 17:46:55 -0600

    Removed an invalid assertion

--- src/account.cc
+++ src/account.cc
@@ -137,7 +137,10 @@ void account_t::add_post(post_t * post)
 
 bool account_t::remove_post(post_t * post)
 {
-  assert(! posts.empty());
+  // It's possible that 'post' wasn't yet in this account, but try to
+  // remove it anyway.  This can happen if there is an error during
+  // parsing, when the posting knows what it's account is, but
+  // xact_t::finalize has not yet added that posting to the account.
   posts.remove(post);
   post->account = NULL;
   return true;
