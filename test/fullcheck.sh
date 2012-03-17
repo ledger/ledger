06934e8dd68a0dea1f113911b01dc6b7ed406330
John Wiegley <johnw@newartisans.com>
2012-03-17 16:22:03 -0500

    libgmalloc is no longer available on OS X Lion

--- test/fullcheck.sh
+++ test/fullcheck.sh
@@ -5,14 +5,14 @@ if [ -x /usr/bin/valgrind -o -x /opt/local/bin/valgrind ]; then
   VALGRIND=valgrind
 fi
 
-export MallocGuardEdges=1
-export MallocScribble=1
-export MallocPreScribble=1
-export MallocCheckHeapStart=100
-export MallocCheckHeapEach=100
-export DYLD_INSERT_LIBRARIES=/usr/lib/libgmalloc.dylib
-export MALLOC_PROTECT_BEFORE=1
-export MALLOC_FILL_SPACE=1
-export MALLOC_STRICT_SIZE=1
+#export MallocGuardEdges=1
+#export MallocScribble=1
+#export MallocPreScribble=1
+#export MallocCheckHeapStart=100
+#export MallocCheckHeapEach=100
+#export DYLD_INSERT_LIBRARIES=/usr/lib/libgmalloc.dylib
+#export MALLOC_PROTECT_BEFORE=1
+#export MALLOC_FILL_SPACE=1
+#export MALLOC_STRICT_SIZE=1
 
 exec $VALGRIND $@
