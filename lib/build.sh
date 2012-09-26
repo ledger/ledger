300cefd1a78ebeddb139a32fa7e1478a3f4225b4
John Wiegley <johnw@newartisans.com>
2012-09-26 12:25:26 -0500

    Changes to support using homebrew and ninja

--- lib/build.sh
+++ lib/build.sh
@@ -4,7 +4,7 @@
 # clang-3.1 from MacPorts.  I build my own Boost instead of using MacPorts'
 # Boost in order to get better debugging support, and to link with libc++.
 
-export PATH=$PATH:/opt/local/lib/openmpi/bin
+#export PATH=$PATH:/opt/local/lib/openmpi/bin
 
 cat > ~/user-config.jam <<EOF
 using clang-darwin : : "/usr/local/bin/clang++" : <cxxflags>-std=c++11 ;
@@ -14,4 +14,4 @@ EOF
 # instead of /usr/local/lib/libc++.1.dylib
 make CXX=clang++ LD=clang++ CC=clang OPTJ=-j20                                 \
      BOOST_TOOLSET=clang-darwin DIR_SUFFIX=clang31                             \
-     BOOST_DEFINES="-sHAVE_ICONV=1 -sICONV_PATH=/usr/local -sHAVE_ICU=1 -sICU_PATH=/usr/local cxxflags=\"-g -std=c++11 $* -nostdlibinc -isystem /usr/local/include -isystem /opt/local/include -isystem /usr/local/include/c++/v1 -isystem /usr/include -stdlib=libc++\" linkflags=\"-g $* -L/usr/local/lib -L/opt/local/lib -L/usr/lib /usr/local/lib/libc++.dylib -stdlib=libc++\""
+     BOOST_DEFINES="-sHAVE_ICONV=1 -sHAVE_ICU=1 -sICU_PATH=/usr/local/opt/icu4c cxxflags=\"-g -std=c++11 $* -nostdlibinc -isystem /usr/local/include -isystem /usr/local/include/c++/v1 -isystem /usr/include -stdlib=libc++\" linkflags=\"-g $* -L/usr/local/lib -L/usr/lib /usr/local/lib/libc++.dylib -stdlib=libc++\""
--- tools/build.sh
+++ tools/build.sh
@@ -6,10 +6,10 @@ shift 1
 JOBS=-j$(sysctl -n hw.activecpu)
 OPTIONS="$flavor --debug --python --doxygen $JOBS"
 
-time (                                          \
-    cd ~/src/ledger ;                           \
-    PATH=/usr/local/bin:/opt/local/bin:$PATH    \
-      nice -n 20 ./acprep $OPTIONS make "$@" && \
-    PATH=/usr/local/bin:/opt/local/bin:$PATH    \
-      nice -n 20 ./acprep $OPTIONS check "$@"   \
+time (                                                  \
+    cd ~/src/ledger ;                                   \
+    PATH=/usr/local/bin:/opt/local/bin:$PATH            \
+      nice -n 20 ./acprep $OPTIONS make $JOBS "$@" &&   \
+    PATH=/usr/local/bin:/opt/local/bin:$PATH            \
+      nice -n 20 ./acprep $OPTIONS check $JOBS "$@"     \
 )
