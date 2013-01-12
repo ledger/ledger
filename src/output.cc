450fd43180251c2cc4e8e822cb36e02791550db6
Craig Earls <enderw88@gmail.com>
2013-01-11 20:39:17 -0700

    Cleaned up the tag output routine based on suggestions from kljohann

--- src/output.cc
+++ src/output.cc
@@ -332,13 +332,10 @@ void report_tags::flush()
 void report_tags::operator()(post_t& post)
 {
   if(post.metadata){
-    for(item_t::string_map::const_iterator post_tags = post.metadata->begin();
-	post_tags!=post.metadata->end();
-	++post_tags){
-      string tag=post_tags->first;
-      if(report.HANDLED(values) && (post_tags->second).first){
-	value_t data=(post_tags->second).first.get();
-	tag+=": "+data.to_string();
+    foreach (const item_t::string_map::value_type& data, *post.metadata){
+      string tag=data.first;
+      if(report.HANDLED(values) && (data.second).first){
+	tag+=": "+ (data.second).first.get().to_string();
       }
       std::map<string, std::size_t>::iterator i = tags.find(tag);
       if (i == tags.end())
@@ -348,7 +345,6 @@ void report_tags::operator()(post_t& post)
     }
   }
 }
-
 
 void report_commodities::flush()
 {
