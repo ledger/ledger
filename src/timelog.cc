bc5563289cd838f1bb33b86706927fae2e0d670f
John Wiegley <johnw@fpcomplete.com>
2013-02-05 00:08:42 -0600

    Fixed bug with --day-break option

--- src/timelog.cc
+++ src/timelog.cc
@@ -169,10 +169,8 @@ void time_log_t::close()
 
     foreach (account_t * account, accounts) {
       DEBUG("timelog", "Clocking out from account " << account->fullname());
-      clock_out_from_timelog(time_xacts,
-                             time_xact_t(none, CURRENT_TIME(), account),
-                             context);
-      context.count++;
+      context.count += clock_out_from_timelog
+        (time_xacts, time_xact_t(none, CURRENT_TIME(), account), context);
     }
     assert(time_xacts.empty());
   }
