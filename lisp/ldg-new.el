fd2c6d87a2353b5cc116bcb886a6c18abb308438
Craig Earls <enderw88@gmail.com>
2013-02-21 22:21:13 -0700

    Added ledger-mode-dump-variables to give me some instrumentation on users installs

--- lisp/ldg-new.el
+++ lisp/ldg-new.el
@@ -89,6 +89,57 @@
           (delete-char 3)
           (forward-line 1))))))
 
+(defun ledger-dump-variable (var)
+  
+  (insert (format "%s: %S\n" (symbol-name var) (eval var))))
+
+(defun ledger-mode-dump-variables ()
+  (interactive)
+  (find-file "ledger-mode-dump")
+  (delete-region (point-min) (point-max))
+  (insert "Ledger Mode Configuration Dump\n")
+  (insert "Date: " (current-time-string) "\n")
+  (insert "Emacs: " (version) "\n")
+  (insert "System Configuration: "system-configuration "\n")
+  (insert "ldg-commodities:\n")
+  (ledger-dump-variable 'ledger-use-decimal-comma)
+  (ledger-dump-variable 'ledger-reconcile-default-commodity)
+  (insert "ldg-exec:\n")
+  (ledger-dump-variable 'ledger-works)
+  (ledger-dump-variable 'ledger-binary-path)
+  (insert "ldg-occur:\n")
+  (ledger-dump-variable 'ledger-occur-use-face-unfolded)
+  (ledger-dump-variable 'ledger-occur-mode)
+  (ledger-dump-variable 'ledger-occur-history)
+  (ledger-dump-variable 'ledger-occur-last-match)
+  (insert "ldg-post:\n")
+  (ledger-dump-variable 'ledger-post-auto-adjust-amounts)
+  (ledger-dump-variable 'ledger-post-amount-alignment-column)
+  (ledger-dump-variable 'ledger-post-use-iswitchb)
+  (ledger-dump-variable 'ledger-post-use-ido)
+  (insert "ldg-reconcile:\n")
+  (ledger-dump-variable 'ledger-recon-buffer-name)
+  (ledger-dump-variable 'ledger-fold-on-reconcile)
+  (ledger-dump-variable 'ledger-buffer-tracks-reconcile-buffer)
+  (ledger-dump-variable 'ledger-reconcile-force-window-bottom)
+  (ledger-dump-variable 'ledger-reconcile-toggle-to-pending)
+  (insert "ldg-register:\n")
+  (ledger-dump-variable 'ledger-register-date-format)
+  (ledger-dump-variable 'ledger-register-line-format)
+  (insert "ldg-reports:\n")
+  (ledger-dump-variable 'ledger-reports)
+(ledger-dump-variable 'ledger-report-format-specifiers)
+(ledger-dump-variable 'ledger-report-buffer-name)
+(insert "ldg-state:")
+(ledger-dump-variable 'ledger-clear-whole-transactions)
+(insert "ldg-xact:\n")
+(ledger-dump-variable 'ledger-highlight-xact-under-point)
+ 
+
+)
+  
+ 
 (provide 'ledger)
 
 ;;; ldg-new.el ends here
+
