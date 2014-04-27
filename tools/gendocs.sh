10359fd7014e5d33fdc6ee7cba6d98b10054cecc
Alexis Hildebrandt <afh@surryhill.net>
2014-04-27 10:00:04 +0200

    gendocs: add option to choose pdf papersize
    The default will be US Letter
    
    [ci skip]

--- tools/gendocs.sh
+++ tools/gendocs.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# By default US Letter is used as the PDF papersize.
+# For those preferring other dimensions add a4 or small
+# as a commandline argument to this script to create a
+# DIN A4 or smallbook version of the PDF.
+case $1 in
+  a4*|afour*)
+    papersize='--texinfo=@afourpaper';;
+  small*)
+    papersize='--texinfo=@smallbook';;
+  *)
+    papersize='';; # US Letter is texinfo default
+esac
+
 echo "===================================== Making Info..."
 makeinfo ledger3.texi
 echo "===================================== Making PDF..."
-texi2pdf --quiet --batch ledger3.texi
+texi2pdf --quiet --batch ${papersize} ledger3.texi
