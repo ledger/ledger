ledger/ledger

commit aa2ff2b5db4afd28fc05e4cb0848d6cc1f0862e9

Improve an error message relating to balance assertions

src/textual.cc
@@ -1609,21 +1609,24 @@ post_t * instance_t::parse_post(char *          line,
             "line " << context.linenum << ": " << "post amount = " << amt);
 
       amount_t diff = amt;
+      amount_t tot;
 
       switch (account_total.type()) {
       case value_t::AMOUNT:
-        diff -= account_total.as_amount();
+        tot = account_total.as_amount();
         break;
 
       case value_t::BALANCE:
         if (optional<amount_t> comm_bal =
             account_total.as_balance().commodity_amount(amt.commodity()))
-          diff -= *comm_bal;
+          tot = *comm_bal;
         break;
 
       default:
         break;
       }
+
+      diff -= tot;
 
       DEBUG("post.assign",
             "line " << context.linenum << ": " << "diff = " << diff);
@@ -1634,7 +1637,9 @@ post_t * instance_t::parse_post(char *          line,
         if (! post->amount.is_null()) {
           diff -= post->amount;
           if (! no_assertions && ! diff.is_zero())
-            throw_(parse_error, _f("Balance assertion off by %1%") % diff);
+            throw_(parse_error,
+                   _f("Balance assertion off by %1% (expected to see %2%)")
+                   % diff % tot);
         } else {
           post->amount = diff;
           DEBUG("textual.parse", "line " << context.linenum << ": "

commit 8b3f8d188444510d8ca00e5db90bac0cf5663c91

Fix bug introduced by commit AA2FF2B Correction is to not use uninitialized amount

src/textual.cc
@@ -1638,24 +1638,23 @@ post_t * instance_t::parse_post(char *          line,
             "line " << context.linenum << ": " << "post amount = " << amt);
 
       amount_t diff = amt;
-      amount_t tot;
 
       switch (account_total.type()) {
       case value_t::AMOUNT:
-        tot = account_total.as_amount();
+        diff -= account_total.as_amount();
         break;
 
       case value_t::BALANCE:
         if (optional<amount_t> comm_bal =
             account_total.as_balance().commodity_amount(amt.commodity()))
-          tot = *comm_bal;
+          diff -= *comm_bal;
         break;
 
       default:
         break;
       }
 
-      diff -= tot;
+      amount_t tot = amt - diff;
 
       DEBUG("post.assign",
             "line " << context.linenum << ": " << "diff = " << diff);

test/regress/AA2FF2B.test
@@ -1,0 +1,10 @@
+2008/01/20 * La Poste
+    Revenu:Invest:Exonéré
+    Actif:Courant:LaPosteLivretA         25,24 € = 25,24 €
+
+test --args-only --decimal-comma bal
+             25,24 €  Actif:Courant:LaPosteLivretA
+            -25,24 €  Revenu:Invest:Exonéré
+--------------------
+                   0
+end test