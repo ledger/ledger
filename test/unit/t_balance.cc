e94efddfb60bd07525865658b83c324de446fe7d
tripun <tripun@gmail.com>
2014-08-11 01:17:08 +0530

    add unit test Ceiling for balance_t

--- test/unit/t_balance.cc
+++ test/unit/t_balance.cc
@@ -340,4 +340,47 @@ BOOST_AUTO_TEST_CASE(testAbs)
   BOOST_CHECK(b2.valid());
 }
 
+BOOST_AUTO_TEST_CASE(testCeiling)
+{
+  amount_t a1("0.00");
+  amount_t a2("$ 123.123");
+  amount_t a3("EUR 456.56");
+  amount_t a4(-a1);
+  amount_t a5(-a2);
+  amount_t a6(-a3);
+
+  balance_t b0;
+  balance_t b1;
+  balance_t b2;
+  balance_t b3;
+  balance_t b4;
+
+  b1 += a1;
+  b1 += a2;
+  b1 += a3;
+  b2 += -a1;
+  b2 += -a2;
+  b2 += -a3;
+
+  b3 += a1.ceilinged();
+  b3 += a2.ceilinged();
+  b3 += a3.ceilinged();
+  b4 += a4.ceilinged();
+  b4 += a5.ceilinged();
+  b4 += a6.ceilinged();
+
+  BOOST_CHECK_EQUAL(b0.ceilinged(), b0);
+  BOOST_CHECK_EQUAL(b2.ceilinged(), b4);
+  BOOST_CHECK_EQUAL(b1.ceilinged(), b3);
+
+  b1.in_place_ceiling();
+  BOOST_CHECK_EQUAL(b1, b3);
+
+  BOOST_CHECK(b0.valid());
+  BOOST_CHECK(b1.valid());
+  BOOST_CHECK(b2.valid());
+  BOOST_CHECK(b3.valid());
+  BOOST_CHECK(b4.valid());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
