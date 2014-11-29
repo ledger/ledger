74ef15bed08bd4a20970b0fcdf4948cb49546059
Alexis Hildebrandt <afh@surryhill.net>
2014-11-29 16:09:59 +0100

    Add basic python unit tests

--- test/python/PostingTest.py
+++ test/python/PostingTest.py
@@ -10,13 +10,35 @@ from datetime import *
 
 class PostingTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        journal = read_journal_from_string("""
+2012-03-01 * (2) KFC
+    Expenses:Food      $21.34
+    Assets:Cash
+""")
+        self.posting = journal.query("food")[0]
 
     def tearDown(self):
         pass
 
-    def test_(self):
-        pass
+    def testType(self):
+        self.assertEqual(type(self.posting), Posting)
+
+    def testDate(self):
+        self.assertEqual(self.posting.date, date(2012, 3, 1))
+
+    def testAccountName(self):
+        self.assertEqual(self.posting.account.name, 'Food')
+        self.assertEqual(str(self.posting.account), 'Expenses:Food')
+ 
+    def testAccountFullame(self):
+        self.assertEqual(self.posting.account.fullname(), 'Expenses:Food')
+ 
+    def testAccountParent(self):
+        self.assertEqual(self.posting.account.parent.name, 'Expenses')
+        self.assertEqual(str(self.posting.account.parent), 'Expenses')
+ 
+    def testAmount(self):
+        self.assertEqual(self.posting.amount, '$21.34')
  
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(PostingTestCase)
--- test/python/TransactionTest.py
+++ test/python/TransactionTest.py
@@ -10,14 +10,34 @@ from datetime import *
 
 class JournalTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        journal = read_journal_from_string("""
+2012-03-01 * (2) KFC
+    Expenses:Food      $21.34
+    Assets:Cash
+""")
+        self.xact = [xact for xact in journal.xacts()][0]
 
     def tearDown(self):
         pass
 
-    def test_(self):
-        pass
- 
+    def testType(self):
+        self.assertEqual(type(self.xact), Transaction)
+
+    def testDate(self):
+        self.assertEqual(self.xact.date, date(2012, 3, 1))
+
+    def testPayee(self):
+        self.assertEqual(self.xact.payee, "KFC")
+
+    def testCode(self):
+        self.assertEqual(self.xact.code, "2")
+
+    def testToString(self):
+        # NOTE: There is a TODO in src/py_xact.cc to change
+        # the string representation of a Transaction to something
+        # more meaningful than an empty string. What should it be?
+        self.assertEqual(str(self.xact), "")
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(JournalTestCase)
 
