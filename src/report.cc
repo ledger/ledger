7bfbecd3ffd03c92eb37331e4c08b3bc1edc784f
Tim Landscheidt <tim@tim-landscheidt.de>
2019-01-12 21:43:14 +0000

    Use HAVE_IOCTL only in report.cc and select.cc

--- src/report.cc
+++ src/report.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2018, John Wiegley.  All rights reserved.
+ * Copyright (c) 2003-2019, John Wiegley.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -49,6 +49,10 @@
 #include "ptree.h"
 #include "emacs.h"
 #include "org.h"
+
+#ifdef HAVE_IOCTL
+#include <sys/ioctl.h>
+#endif
 
 namespace ledger {
 
--- src/select.cc
+++ src/select.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2018, John Wiegley.  All rights reserved.
+ * Copyright (c) 2003-2019, John Wiegley.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -41,6 +41,10 @@
 #include "filters.h"
 #include "scope.h"
 #include "op.h"
+
+#ifdef HAVE_IOCTL
+#include <sys/ioctl.h>
+#endif
 
 namespace ledger {
 
