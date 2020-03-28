0b0728e2a7e08194552624608770a770f653f767
John Wiegley <johnw@newartisans.com>
2020-03-27 18:28:35 -0700

    Update default.nix to match more closely what nixpkgs is using

--- default.nix
+++ default.nix
@@ -1,35 +1,44 @@
-{ stdenv, fetchFromGitHub, cmake, boost, gmp, mpfr, libedit, python
-, texinfo, gnused, usePython ? true }:
+{ packages ? "llvmPackages_9"
 
-stdenv.mkDerivation rec {
-  name = "ledger-${version}";
+, rev    ? "1fe82110febdf005d97b2927610ee854a38a8f26"
+, sha256 ? "08x6saa7iljyq2m0j6p9phy0v17r3p8l7vklv7y7gvhdc7a85ppi"
+
+, pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256; }) {
+    config.allowUnfree = true;
+    config.allowBroken = false;
+    overlays = [
+      (self: super: {})
+    ];
+  }
+}:
+
+let
   version = "3.1.3";
+  rev = "20190331";
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "ledger-${version}-${rev}";
 
   src = ./.;
 
-  buildInputs = [
-    (boost.override { enablePython = usePython; })
-    gmp mpfr libedit python texinfo gnused
-  ];
-
-  nativeBuildInputs = [ cmake ];
+  buildInputs = with pkgs; [ cmake boost gmp mpfr libedit python texinfo gnused ];
 
   enableParallelBuilding = true;
 
-  cmakeFlags = [
-    "-DCMAKE_INSTALL_LIBDIR=lib"
-    # "-DBUILD_DOCS:BOOL=ON"
-    (stdenv.lib.optionalString usePython "-DUSE_PYTHON=true")
-   ];
+  cmakeFlags = [ "-DCMAKE_INSTALL_LIBDIR=lib" ];
 
-  postBuild = ''
-    make doc
-  '';
+  buildPhase = "make -j$NIX_BUILD_CORES";
+  checkPhase = "ctest -j$NIX_BUILD_CORES";
 
-  meta = with stdenv.lib; {
-    homepage = https://ledger-cli.org/;
+  doCheck = true;
+
+  meta = {
+    homepage = "http://ledger-cli.org/";
     description = "A double-entry accounting system with a command-line reporting interface";
-    license = licenses.bsd3;
+    license = pkgs.stdenv.lib.licenses.bsd3;
 
     longDescription = ''
       Ledger is a powerful, double-entry accounting system that is accessed
@@ -38,7 +47,7 @@ stdenv.mkDerivation rec {
       their data, there really is no alternative.
     '';
 
-    platforms = platforms.all;
-    maintainers = with maintainers; [ the-kenny jwiegley ];
+    platforms = pkgs.stdenv.lib.platforms.all;
+    maintainers = with pkgs.stdenv.lib.maintainers; [ the-kenny jwiegley ];
   };
 }
