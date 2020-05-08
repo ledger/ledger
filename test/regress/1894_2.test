ledger/ledger

commit 49b07a1c19489547b859d61fbc5c240aff224dda

Correction to the way parens are parsed in query expressions

src/query.cc
@@ -36,6 +36,17 @@
 
 namespace ledger {
 
+bool query_t::lexer_t::advance()
+{
+  if (begin == end || ++begin == end) {
+    return false;
+  } else {
+    arg_i   = (*begin).as_string().begin();
+    arg_end = (*begin).as_string().end();
+  }
+  return true;
+}
+
 query_t::lexer_t::token_t
 query_t::lexer_t::next_token(query_t::lexer_t::token_t::kind_t tok_context)
 {
@@ -46,12 +57,8 @@ query_t::lexer_t::next_token(query_t::lexer_t::token_t::kind_t tok_context)
   }
 
   if (arg_i == arg_end) {
-    if (begin == end || ++begin == end) {
+    if (! advance())
       return token_t(token_t::END_REACHED);
-    } else {
-      arg_i   = (*begin).as_string().begin();
-      arg_end = (*begin).as_string().end();
-    }
   }
 
  resume:
@@ -153,9 +160,6 @@ query_t::lexer_t::next_token(query_t::lexer_t::token_t::kind_t tok_context)
         break;
 
       case ')':
-        if (! consume_next && tok_context == token_t::TOK_EXPR)
-          goto test_ident;
-        // fall through...
       case '(':
       case '&':
       case '|':
@@ -253,6 +257,23 @@ void query_t::lexer_t::token_t::expected(char wanted, char c)
   }
 }
 
+std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+  str.erase(0, str.find_first_not_of(chars));
+  return str;
+}
+
+std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+  str.erase(str.find_last_not_of(chars) + 1);
+  return str;
+}
+
+std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+  return ltrim(rtrim(str, chars), chars);
+}
+
 expr_t::ptr_op_t
 query_t::parser_t::parse_query_term(query_t::lexer_t::token_t::kind_t tok_context)
 {
@@ -275,12 +296,28 @@ query_t::parser_t::parse_query_term(query_t::lexer_t::token_t::kind_t tok_contex
   case lexer_t::token_t::TOK_NOTE:
   case lexer_t::token_t::TOK_ACCOUNT:
   case lexer_t::token_t::TOK_META:
-  case lexer_t::token_t::TOK_EXPR:
     node = parse_query_term(tok.kind);
     if (! node)
       throw_(parse_error,
              _f("%1% operator not followed by argument") % tok.symbol());
     break;
+
+  case lexer_t::token_t::TOK_EXPR: {
+    string arg(lexer.arg_i, lexer.arg_end);
+    lexer.arg_i = lexer.arg_end;
+    trim(arg);
+
+    if (arg.length() == 0) {
+      if (lexer.advance()) {
+        arg = string(lexer.arg_i, lexer.arg_end);
+        lexer.arg_i = lexer.arg_end;
+        trim(arg);
+      }
+    }
+
+    node = expr_t(arg).get_op();
+    break;
+  }
 
   case lexer_t::token_t::TERM:
     assert(tok.value);

src/query.h
@@ -223,6 +223,7 @@ public:
       TRACE_DTOR(query_t::lexer_t);
     }
 
+    bool    advance();
     token_t next_token(token_t::kind_t tok_context = token_t::UNKNOWN);
     void    push_token(token_t tok) {
       assert(token_cache.kind == token_t::UNKNOWN);

commit 64feba2af72f3d2ceb175a4cfb9573dc9732ec31

Add regression tests for issue #1894
Add tests for wrong behavior caused by commit 49b07a1c1948 ("Correction
to the way parens are parsed in query expressions") which was reverted
with commit 869302ae9ce3.  The bug report was issue #1894.

test/regress/1894_1.test
@@ -1,0 +1,16 @@
+= /Expenses:Transportation/ and not expr "has_tag(/NOTAX/)"
+         Expenses:Tax                     0.2
+         $account                        -0.2
+
+2020-05-06 * Test
+    Expenses:Transportation            200.00 EUR
+    Assets:Bank                       -200.00 EUR
+
+test bal
+         -200.00 EUR  Assets:Bank
+          200.00 EUR  Expenses
+           40.00 EUR    Tax
+          160.00 EUR    Transportation
+--------------------
+                   0
+end test

test/regress/1894_2.test
@@ -1,0 +1,21 @@
+= ^Expenses and expr "any(account =~ /^A:Assets/ and R)"
+    [A:Assets:Split]              (amount / 2)
+    [B:Assets:Split]              (-amount / 2)
+
+= ^Expenses and expr "any(account =~ /^B:Assets/ and R)"
+    [B:Assets:Split]              (amount / 2)
+    [A:Assets:Split]              (-amount / 2)
+
+2020-05-08 * Test
+    Expenses:Transportation            200.00 EUR
+    A:Assets:Checking
+
+test bal
+         -100.00 EUR  A:Assets
+         -200.00 EUR    Checking
+          100.00 EUR    Split
+         -100.00 EUR  B:Assets:Split
+          200.00 EUR  Expenses:Transportation
+--------------------
+                   0
+end test