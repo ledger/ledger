0991356b73c1dfd0601e525e353462ba471d61c6
Nemanja Trifunovic <ntrifunovic@hotmail.com>
2021-04-30 14:58:30 -0400

    find_invalid(string_view) returns string_view::npos, not string::npos

--- source/utf8/cpp17.h
+++ source/utf8/cpp17.h
@@ -66,7 +66,7 @@ namespace utf8
     inline std::size_t find_invalid(std::string_view s)
     {
         std::string_view::const_iterator invalid = find_invalid(s.begin(), s.end());
-        return (invalid == s.end()) ? std::string::npos : (invalid - s.begin());
+        return (invalid == s.end()) ? std::string_view::npos : (invalid - s.begin());
     }
 
     inline bool is_valid(std::string_view s)
