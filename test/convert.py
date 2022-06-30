18305fd30df731e27b6e909ea7d846b579f0abc4
Alexis Hildebrandt <afh@surryhill.net>
2022-06-30 14:30:21 +0200

    Replace os.path.abspath with os.path.realpath
    to fix failing tests on Darwin, where /tmp is a symlink to /private/tmp
    and the tests fail as ledger reports filenames with the symlink resolved
    to /private/tmp, but the tests expect files with /tmp.

--- test/convert.py
+++ test/convert.py
@@ -36,9 +36,9 @@ import re
 import sys
 import os
 
-source = os.path.abspath(sys.argv[1])
+source = os.path.realpath(sys.argv[1])
 base   = os.path.splitext(source)[0]
-target = os.path.abspath(sys.argv[2])
+target = os.path.realpath(sys.argv[2])
 
 dirname = os.path.dirname(target)
 if not os.path.isdir(dirname):
