0ade3a55fb84f5d899bcdac3f92dd9af6b370c66
David Sklar <177495+davidsklar@users.noreply.github.com>
2022-12-29 13:45:37 -0500

    Python: Transaction.remove_post should call xact_base_t::remove_post

--- test/python/TransactionTest.py
+++ test/python/TransactionTest.py
@@ -6,18 +6,33 @@ import operator
 from ledger import *
 from datetime import *
 
-class JournalTestCase(unittest.TestCase):
+class TransactionTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.journal = read_journal_from_string("""
+2012-03-01 KFC
+    Expenses:Food      $21.34
+    Assets:Cash
+2012-03-02 MJT
+    Expenses:Museum      $45.67
+    Assets:Cash
+""")
 
     def tearDown(self):
         close_journal_files()
 
-    def test_(self):
-        pass
- 
+    def testAddRemovePosts(self):
+        xacts = [xact for xact in self.journal]
+        x1_posts = [post for post in xacts[1]]
+        for post in x1_posts:
+            xacts[0].add_post(post)
+            xacts[1].remove_post(post)
+        x0_posts = [post for post in xacts[0]]
+        x1_posts = [post for post in xacts[1]]
+        self.assertEqual(len(x0_posts), 4)
+        self.assertEqual(len(x1_posts), 0)
+
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(JournalTestCase)
+    return unittest.TestLoader().loadTestsFromTestCase(TransactionTestCase)
 
 if __name__ == '__main__':
     unittest.main()
