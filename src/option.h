6936c2c4309b839480326df916aad60f4f986a24
Alexis Hildebrandt <afh@surryhill.net>
2023-02-01 06:37:29 +0100

    Update copyright year
    [skip ci]

--- src/option.h
+++ src/option.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2022, John Wiegley.  All rights reserved.
+ * Copyright (c) 2003-2023, John Wiegley.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
