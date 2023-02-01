dd2fde2aa87372189e2548ef556b50a0b8ee45f2
Alexis Hildebrandt <afh@surryhill.net>
2023-02-01 15:29:14 +0800

    Updated lib/utfcpp
    * Squashed 'lib/utfcpp/' content from commit 79835a5fa
      git-subtree-dir: lib/utfcpp
      git-subtree-split: 79835a5fa57271f07a90ed36123e30ae9741178e
    * Change include path to utfcpp

--- lib/utfcpp/v3/samples/docsample.cpp
+++ lib/utfcpp/v3/samples/docsample.cpp
@@ -17,8 +17,8 @@ int main(int argc, char** argv)
     // Open the test file (must be UTF-8 encoded)
     ifstream fs8(test_file_path);
     if (!fs8.is_open()) {
-    cout << "Could not open " << test_file_path << endl;
-    return 0;
+        cout << "Could not open " << test_file_path << endl;
+        return 0;
     }
 
     unsigned line_count = 1;
@@ -26,21 +26,33 @@ int main(int argc, char** argv)
     // Play with all the lines in the file
     while (getline(fs8, line)) {
         // check for invalid utf-8 (for a simple yes/no check, there is also utf8::is_valid function)
+#if __cplusplus >= 201103L // C++ 11 or later
+        auto end_it = utf8::find_invalid(line.begin(), line.end());
+#else
         string::iterator end_it = utf8::find_invalid(line.begin(), line.end());
+#endif // C++ 11
         if (end_it != line.end()) {
             cout << "Invalid UTF-8 encoding detected at line " << line_count << "\n";
             cout << "This part is fine: " << string(line.begin(), end_it) << "\n";
         }
         // Get the line length (at least for the valid part)
-        int length = utf8::distance(line.begin(), end_it);
+        ptrdiff_t length = utf8::distance(line.begin(), end_it);
         cout << "Length of line " << line_count << " is " << length <<  "\n";
 
         // Convert it to utf-16
+#if __cplusplus >= 201103L // C++ 11 or later
+        u16string utf16line = utf8::utf8to16(line);
+#else
         vector<unsigned short> utf16line;
         utf8::utf8to16(line.begin(), end_it, back_inserter(utf16line));
+#endif // C++ 11
         // And back to utf-8;
+#if __cplusplus >= 201103L // C++ 11 or later
+        string utf8line = utf8::utf16to8(utf16line);
+#else
         string utf8line; 
         utf8::utf16to8(utf16line.begin(), utf16line.end(), back_inserter(utf8line));
+#endif // C++ 11
         // Confirm that the conversion went OK:
         if (utf8line != string(line.begin(), end_it))
             cout << "Error in UTF-16 conversion at line: " << line_count << "\n";        
---
+++ lib/utfcpp/v3/tests/test_checked_iterator.h
@@ -0,0 +1,35 @@
+#ifndef UTF8_FOR_CPP_TEST_CHECKED_ITERATOR_H_2675DCD0_9480_4c0c_B92A_CC14C027B731
+#define UTF8_FOR_CPP_TEST_CHECKED_ITERATOR_H_2675DCD0_9480_4c0c_B92A_CC14C027B731
+
+#include "utf8.h"
+
+using namespace utf8;
+
+
+TEST(CheckedIteratrTests, test_increment)
+{
+    const char* threechars = "\xf0\x90\x8d\x86\xe6\x97\xa5\xd1\x88";
+    utf8::iterator<const char*> it(threechars, threechars, threechars + 9);
+    utf8::iterator<const char*> it2 = it;
+    EXPECT_EQ (it2, it);
+    EXPECT_EQ (*it, 0x10346);
+    EXPECT_EQ (*(++it), 0x65e5);
+    EXPECT_EQ ((*it++), 0x65e5);
+    EXPECT_EQ (*it, 0x0448);
+    EXPECT_NE (it, it2);
+    utf8::iterator<const char*> endit (threechars + 9, threechars, threechars + 9);
+    EXPECT_EQ (++it, endit);
+}
+
+TEST(CheckedIteratrTests, test_decrement)
+{
+    const char* threechars = "\xf0\x90\x8d\x86\xe6\x97\xa5\xd1\x88";
+    utf8::iterator<const char*> it(threechars+9, threechars, threechars + 9);
+    EXPECT_EQ (*(--it), 0x0448);
+    EXPECT_EQ ((*it--), 0x0448);
+    EXPECT_EQ (*it, 0x65e5);
+    EXPECT_EQ (--it, utf8::iterator<const char*>(threechars, threechars, threechars + 9));
+    EXPECT_EQ (*it, 0x10346);
+}
+
+#endif
