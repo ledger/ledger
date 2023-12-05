3c6006e8967093963dbbdb128d56baca6c097127
Alexis Hildebrandt <afh@surryhill.net>
2023-12-05 09:15:18 +0100

    test: Include CheckManpage and CheckTexinfo in tests

--- test/CheckManpage.py
+++ test/CheckManpage.py
@@ -13,15 +13,16 @@ from CheckOptions import CheckOptions
 class CheckManpage (CheckOptions):
   def __init__(self, args):
     CheckOptions.__init__(self, args)
-    self.option_pattern = '\.It Fl \\\\-([-A-Za-z]+)'
-    self.function_pattern = '\.It Fn ([-A-Za-z_]+)'
+    self.option_pattern = r'^\.It Fl \\-([-A-Za-z]+)'
+    self.function_pattern = r'^\.It Fn ([-A-Za-z_]+)'
+    self.symbol_pattern = r'^\.It Sy ([-A-Za-z_]+)'
     self.source_file = join(self.source, 'doc', 'ledger.1')
     self.source_type = 'manpage'
 
 if __name__ == "__main__":
   args = argparse.ArgumentParser(prog='CheckManpage',
-                                 description='Check that ledger options are documented in the manpage'
-                                 parents=[CheckOptions.parser()]).parse_args()
+                                 description='Check that ledger options are documented in the manpage',
+                                 parents=[CheckManpage.parser()]).parse_args()
   script = CheckManpage(args)
   status = script.main()
   sys.exit(status)
