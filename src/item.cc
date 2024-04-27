d87dee4f3ad41ba51bb91b2e54cec54391378dc6
Taylor R Campbell <campbell+ledger@mumble.net>
2024-04-27 19:54:23 +0000

    Avoid ctype abuse.
    fix https://github.com/ledger/ledger/issues/2338
    fix https://github.com/ledger/ledger/issues/2340

--- src/item.cc
+++ src/item.cc
@@ -157,7 +157,8 @@ void item_t::parse_tags(const char * p,
   if (! std::strchr(p, ':')) {
     if (const char * b = std::strchr(p, '[')) {
       if (*(b + 1) != '\0' &&
-          (std::isdigit(*(b + 1)) || *(b + 1) == '=')) {
+          (std::isdigit(static_cast<unsigned char>(*(b + 1))) ||
+           *(b + 1) == '=')) {
         if (const char * e = std::strchr(b, ']')) {
           char buf[256];
           std::strncpy(buf, b + 1, static_cast<std::size_t>(e - b - 1));
--- src/times.cc
+++ src/times.cc
@@ -1477,7 +1477,7 @@ date_parser_t::lexer_t::token_t date_parser_t::lexer_t::next_token()
     return tok;
   }
 
-  while (begin != end && std::isspace(*begin))
+  while (begin != end && std::isspace(static_cast<unsigned char>(*begin)))
     begin++;
 
   if (begin == end)
@@ -1496,9 +1496,11 @@ date_parser_t::lexer_t::token_t date_parser_t::lexer_t::next_token()
   // date using the typical date formats.  This allows not only dates like
   // "2009/08/01", but also dates that fit the user's --input-date-format,
   // assuming their format fits in one argument and begins with a digit.
-  if (std::isdigit(*begin)) {
+  if (std::isdigit(static_cast<unsigned char>(*begin))) {
     string::const_iterator i = begin;
-    for (i = begin; i != end && ! std::isspace(*i); i++) {}
+    for (i = begin;
+         i != end && ! std::isspace(static_cast<unsigned char>(*i));
+         i++) {}
     assert(i != begin);
 
     string possible_date(start, i);
@@ -1523,18 +1525,20 @@ date_parser_t::lexer_t::token_t date_parser_t::lexer_t::next_token()
   start = begin;
 
   string term;
-  bool alnum = std::isalnum(*begin);
-  for (; (begin != end && ! std::isspace(*begin) &&
-          ((alnum && static_cast<bool>(std::isalnum(*begin))) ||
-           (! alnum && ! static_cast<bool>(std::isalnum(*begin))))); begin++)
+  bool alnum = std::isalnum(static_cast<unsigned char>(*begin));
+  for (; (begin != end && ! std::isspace(static_cast<unsigned char>(*begin)) &&
+          ((alnum && static_cast<bool>(std::isalnum(
+             static_cast<unsigned char>(*begin)))) ||
+           (! alnum && ! static_cast<bool>(std::isalnum(
+             static_cast<unsigned char>(*begin)))))); begin++)
     term.push_back(*begin);
 
   if (! term.empty()) {
-    if (std::isdigit(term[0])) {
+    if (std::isdigit(static_cast<unsigned char>(term[0]))) {
       return token_t(token_t::TOK_INT,
                      token_t::content_t(lexical_cast<unsigned short>(term)));
     }
-    else if (std::isalpha(term[0])) {
+    else if (std::isalpha(static_cast<unsigned char>(term[0]))) {
       to_lower(term);
 
       if (optional<date_time::months_of_year> month =
