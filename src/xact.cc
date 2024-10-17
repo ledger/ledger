9ac296c70a70c6c341151d34ef248bbcccf7ea22
Sven Püschel <SvenPschel@akarisu.de>
2024-10-17 17:06:39 +0200

    Remove DOCUMENT_MODEL macro
    Remove DOCUMENT_MODEL macro, as it is set to the fixed value 0 in
    ledger.hh.in . Therefore just remove all of the dead code fenced with
    the macro, as probably nobody will change the macro anyways.

--- src/xact.cc
+++ src/xact.cc
@@ -471,9 +471,6 @@ bool xact_base_t::verify()
 
 xact_t::xact_t(const xact_t& e)
   : xact_base_t(e), code(e.code), payee(e.payee)
-#if DOCUMENT_MODEL
-    , data(NULL)
-#endif
 {
   TRACE_CTOR(xact_t, "copy");
 }
