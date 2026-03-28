6b8e1524de9e5a6c9d2c587234355b4e428200ed
Alexis Hildebrandt <afh@surryhill.net>
2026-03-28 09:24:22 +0100

    shell.nix: Add sha256

--- shell.nix
+++ shell.nix
@@ -1,3 +1,9 @@
-(import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
-  src = ./.;
-}).shellNix
+(import
+  (fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+    sha256 = "0yqfa6rx8md81bcn4szfp0hjq2f3h9i8zjzhqqyfqdkrj5559nmw";
+  })
+  {
+    src = ./.;
+  }
+).shellNix
